3a1c6f82556c79440e8f731ad05cd354c273601b
H4ppy <95917527+H4ppYEndLess@users.noreply.github.com>
2023-11-04 22:46:37 +0200

    chat fsola

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,32 @@
 (() => {
-    const refs = {
+  const refs = {
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
-      body: document.body
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
-  
+  };
+
+  // Function to disable scrolling with !important to ensure it takes precedence
+  function disableScroll() {
+      document.body.style.setProperty('overflow', 'hidden', 'important');
+  }
+
+  // Function to enable scrolling
+  function enableScroll() {
+      document.body.style.removeProperty('overflow');
+  }
+
+  // Function to handle opening the modal
+  function openModal() {
+      refs.modal.classList.remove("is-hidden");
+      disableScroll();
+  }
+
+  // Function to handle closing the modal
+  function closeModal() {
+      refs.modal.classList.add("is-hidden");
+      enableScroll();
+  }
+
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+})();
